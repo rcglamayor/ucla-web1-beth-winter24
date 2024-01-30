44abdcf0971ff5b0a2b5e842a76149ac9aba338e
Beth Schore <betro1@gmail.com>
2024-01-29 17:58:39 -0800

    L3: Complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Routes from './Pages/Routes.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>Student Name</React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Routes />
+    </React.StrictMode>
 );
